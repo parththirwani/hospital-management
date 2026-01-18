bde55e09f79bade7d6ec848b4d24f9e8beadd416
parththirwani <124462644+parththirwani@users.noreply.github.com>
2026-01-18 08:28:38 +0530

    get-apis

--- main.py
+++ main.py
@@ -1,30 +1,67 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Path, Query
 import json
 
 def load_data():
     with open("patients.json", "r") as f:
         data = json.load(f)
-        return data
+    return data
 
 app = FastAPI()
 
 @app.get("/")
 def hello():
-    return{"message": "App to store patient data"}
+    return {"message": "App to store patient data"}
 
 @app.get("/about")
 def about():
-    return{
-        "mesasge": "A fully functional API to manage your patient records"
-    }
+    return {"message": "A fully functional API to manage your patient records"}  
 
 
 @app.get("/patients")
-def patients():
+def getPatients():
     data = load_data()
-    return{
-        "mesasge": "Patient data retrieved sucessfully",
+    return {
+        "message": "Patient data retrieved successfully",   
         "data": data
     }
 
+@app.get("/patients/sort")
+def sortPatients(
+    sort_by: str = Query(..., description="Sort on the basis of height, weight, bmi"),
+    order: str = Query("asc", description="Sort by asc or desc order")
+):
+    valid_fields = ["height", "weight", "bmi"]   
 
+    if sort_by not in valid_fields:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Invalid field. Choose one of: {valid_fields}"
+        )
+
+    if order not in ["asc", "desc"]:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid order. Use 'asc' or 'desc'"
+        )
+
+    data = load_data()
+    reverse = order == "desc"
+
+    sorted_data = sorted(
+        data.values(),                          
+        key=lambda x: x.get(sort_by, 0),
+        reverse=reverse
+    )
+
+    return {
+        "message": "Sorted data fetched successfully",
+        "data": sorted_data                      
+    }
+
+@app.get("/patients/{patient_id}")
+def getPatient(patient_id: str = Path(..., description="ID of the patient in DB", example="P001")):
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    else:
+        raise HTTPException(status_code=404, detail="Patient data not found")
